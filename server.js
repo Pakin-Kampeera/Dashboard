257f189152f66ab78a32c827f44bceaba02117f9
Pakin Kampeera <armpakin@hotmail.com>
2021-05-26 15:00:26 +0700

    Remove client UI

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const express = require('express');
 const connectDB = require('./config/db');
 const path = require('path');
 const errorHandler = require('./middleware/error');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
+
 app.use(express.json());
 
 // Connect MongoDB

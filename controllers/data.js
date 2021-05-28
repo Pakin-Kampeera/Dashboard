e122e5e6392360700870541b6b8c3002ef564adb
Pakin Kampeera <armpakin@hotmail.com>
2021-05-29 00:10:20 +0700

    Add: route

--- controllers/data.js
+++ controllers/data.js
@@ -2,4 +2,8 @@ const getData = (req, res, next) => {
   res.status(200).json({ success: true, data: 'You got access to the private data in this route' });
 };
 
-module.exports = getData;
+const getUsers = (req, res, next) => {};
+
+const getSettings = (req, res, next) => {};
+
+module.exports = { getData, getUsers, getSettings };
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,10 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 
-const getData = require('../controllers/data');
+const { getData, getUsers, getSettings } = require('../controllers/data');
 
 const auth = require('../middleware/auth');
 
-router.route('/dashboard').get(auth, getData)
+router.route('/dashboard').get(auth, getData);
+
+router.route('/users').get(auth, getUsers);
+
+router.route('/settings').get(auth, getSettings);
 
 module.exports = router;

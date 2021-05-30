af37310fc1c4f6b3287ad6d11a048c349321994a
Pakin Kampeera <armpakin@hotmail.com>
2021-05-31 01:54:56 +0700

    Add: Socket.io for realtime MongoDB databse

--- controllers/data.js
+++ controllers/data.js
@@ -1,3 +1,22 @@
+const { isValidObjectId } = require('mongoose');
+const Data = require('../models/Data');
+const History = require('../models/History');
+const ErrorResponse = require('../utils/errorResponse');
+
+// const dataChangeStream = Data.watch();
+
+// dataChangeStream.on('change', (change) => {
+//   console.log(change);
+//   io.emit('changeData', change);
+// });
+
+// const historyChangeStream = History.watch()
+
+// historyChangeStream.on('change', (change) => {
+//   console.log(change)
+//   isValidObjectId.emit('changeHistory', change)
+// })
+
 const getData = (req, res, next) => {
   res.status(200).json({ success: true, data: 'You got access to the private data in this route' });
 };
--- server.js
+++ server.js
@@ -1,11 +1,44 @@
 require('dotenv').config({ path: './config.env' });
+
+// Integrate express and socket.io
 const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http, { cors: { origin: '*' } });
+
 const connectDB = require('./config/db');
+const cors = require('cors');
 const path = require('path');
 const errorHandler = require('./middleware/error');
-const app = express();
 
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
 app.use(express.json());
+
+// Connect MongoDB
+connectDB();
+
+// Route setup
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/data', require('./routes/dashboard'));
+
+// Connect socket
+io.on('connection', (socket) => {
+  console.log('Socket connected');
+});
+
+// Error handler middleware
+app.use(errorHandler);
+
+// Serve Restful API
+const server = http.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// Server crash handler
+process.on('unhandledRejection', (error, promise) => {
+  console.log(`Logged Error: ${error}`);
+  server.close(() => process.exit(1));
+});
 
 // Heroku setup
 if (process.env.NODE_ENV === 'production') {
@@ -13,27 +46,4 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
-} else {
-  const cors = require('cors');
-  app.use(cors());
 }
-
-// Connect MongoDB
-connectDB();
-
-// Authentication route
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/data', require('./routes/dashboard'));
-
-// Error handler middleware
-app.use(errorHandler);
-
-// Serve Restful API
-const PORT = process.env.PORT || 5000;
-const server = app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-// Server crash handler
-process.on('unhandledRejection', (error, promise) => {
-  console.log(`Logged Error: ${error}`);
-  server.close(() => process.exit(1));
-});
